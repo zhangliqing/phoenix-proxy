4c5c0af808489f45bb70f6801a62db738f22a18e
zhangliqing <623009470@qq.com>
2017-11-08 21:07:19 +0800

    123

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ wss.on('connection', function connection(ws, req) {
   var pathname = location.pathname;
   var serviceName = pathname.substr(1)
   console.log(serviceName)
-  request.get({url: service.rancher.endpoint + '/services'}, function (err, httpResponse, body) {
+  request.get({url: 'http://117.50.1.134:8080/v2-beta/projects/1a3504' + '/services'}, function (err, httpResponse, body) {
     var parsedServices = JSON.parse(body)
     for (var i = 0; i < parsedServices.data.length; i++) {
       if (parsedServices.data[i].name == serviceName) {
         var instanceId = parsedServices.data[i].instanceIds[0]
         console.log('instanceId: '+instanceId)
-        request.get({url: service.rancher.endpoint + '/containers/' + instanceId}, function (err, httpResponse, body1) {
+        request.get({url: 'http://117.50.1.134:8080/v2-beta/projects/1a3504' + '/containers/' + instanceId}, function (err, httpResponse, body1) {
           var parsedContainer = JSON.parse(body1)
           var containerIp = parsedContainer.primaryIpAddress
           const wsClient = new WebSocket('ws://' + containerIp + ':5678');
