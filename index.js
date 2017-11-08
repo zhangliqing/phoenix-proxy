3d3098faae638e71d4fbf06bef3462235fcfcad9
zhangliqing <623009470@qq.com>
2017-11-09 00:07:26 +0800

    test

--- index.js
+++ index.js
@@ -15,35 +15,39 @@ wss.on('connection', function connection(ws, req) {
     for (var i = 0; i < parsedServices.data.length; i++) {
       if (parsedServices.data[i].name == serviceName) {
         var instanceId = parsedServices.data[i].instanceIds[0]
-        console.log('instanceId: '+instanceId)
+        console.log('instanceId: ' + instanceId)
         request.get({url: 'http://117.50.1.134:8080/v2-beta/projects/1a3504' + '/containers/' + instanceId}, function (err, httpResponse, body1) {
           var parsedContainer = JSON.parse(body1)
           console.log(parsedContainer.primaryIpAddress)
           var containerIp = parsedContainer.primaryIpAddress
-          setTimeout(function () {
-            const wsClient = new WebSocket('ws://' + containerIp + ':5678');
-            wsClient.on('open', function () {
-              ws.on('message', function (message) {
-                wsClient.send(message)
-              });
-              wsClient.on('message', function (data) {
-                ws.send(data)
-              })
-            })
-            wsClient.on('error',function () {
-              ws.close()
-            })
-            wsClient.on('close',function () {
-              ws.close()
-            })
-            ws.on('close',function () {
-              wsClient.close()
-            })
-          },5000)
-
+          connectPulsar(ws, containerIp)
         })
         break
       }
     }
   })
 });
+var connectPulsar = function (ws, containerIp) {
+  const wsClient = new WebSocket('ws://' + containerIp + ':5678');
+  wsClient.on('open', function () {
+    ws.on('message', function (message) {
+      wsClient.send(message)
+    });
+    wsClient.on('message', function (data) {
+      ws.send(data)
+    })
+  })
+  wsClient.on('error', function () {
+    ws.close()
+  })
+  wsClient.on('close', function () {
+    if (ws.readyState == 1) {
+      connectPulsar(ws, containerIp)
+      return
+    }
+    ws.close()
+  })
+  ws.on('close', function () {
+    wsClient.close()
+  })
+}
