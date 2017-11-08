84ed79f743a3d165a9fd329a46660098db605681
zhangliqing <623009470@qq.com>
2017-11-08 22:39:27 +0800

    test

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ wss.on('connection', function connection(ws, req) {
         console.log('instanceId: '+instanceId)
         request.get({url: 'http://117.50.1.134:8080/v2-beta/projects/1a3504' + '/containers/' + instanceId}, function (err, httpResponse, body1) {
           var parsedContainer = JSON.parse(body1)
-          console.log(parsedContainer.primaryIpAddress.primaryIpAddress)
+          console.log(parsedContainer.primaryIpAddress)
           var containerIp = parsedContainer.primaryIpAddress
           const wsClient = new WebSocket('ws://' + containerIp + ':5678');
           wsClient.on('open', function () {
@@ -28,6 +28,12 @@ wss.on('connection', function connection(ws, req) {
             wsClient.on('message', function (data) {
               ws.send(data)
             })
+          })
+          wsClient.on('error',function () {
+            ws.close()
+          })
+          wsClient.on('close',function () {
+            ws.close()
           })
           ws.on('close',function () {
             wsClient.close()
